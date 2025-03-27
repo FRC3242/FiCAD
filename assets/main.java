a0c18266d7fd3b1a6cf0ab6f7197ef237e22f05e
Daniel <dannyeffren@gmail.com>
2025-03-27 18:06:50 -0400

    we have a window :D

--- assets/main.java
+++ assets/main.java
@@ -1,7 +1,32 @@
 package assets;
-//iiiiiiiii
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class main {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
+
+    public main() {
+        JFrame frame = new JFrame();
+        
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("FiCAD Program");
+        frame.pack();
+        frame.setVisible(true);
+
+    }    
+
+    public static void main(String[] args) {
+        main FiCADWindow = new main();
     }
+
 }
